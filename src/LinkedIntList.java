796c16b4ca75ab42f892095ff3b85ef6d2ef8aa8
zerbash <dbasham@overlake.org>
2021-10-05 15:18:14 -0700

    10-5-21

--- src/LinkedIntList.java
+++ src/LinkedIntList.java
@@ -13,4 +13,48 @@ public class LinkedIntList {
             current.next = temp;
         }
     }
+
+    public void add(int index, int value) {
+        if (index == 0){
+            front = new ListNode(value, front);
+        } else {
+            ListNode current = front;
+            for (int i = 0; i < index - 1; i++) {
+                current = current.next;
+            }
+            current.next = new ListNode(value, current.next);
+        }
+
+    }
+
+    public int get(int index) {
+        ListNode current = front;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.data;
+    }
+
+    public int remove() {
+        int value = front.data;
+        front = front.next;
+        return value;
+    }
+
+    public String toString() {
+        String s = "[";
+        if (front == null) {
+            return s += "]";
+        }
+        ListNode current = front;
+        while (current.next != null) {
+            s += current.data;
+            if (current.next.next != null) {
+                s += ", ";
+            }
+            current = current.next;
+        }
+        return s + "]";
+
+    }
 }
---
+++ src/LinkedIntListClient.java
@@ -0,0 +1,9 @@
+public class LinkedIntListClient {
+    public static void main(String[] args) {
+        LinkedIntList list = new LinkedIntList();
+        list.add(42);
+        list.add(-3);
+        list.add(17);
+        System.out.println(list);
+    }
+}
